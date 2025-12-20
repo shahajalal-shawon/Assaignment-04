13b84ab0e24ff352dff47cc2b6c7285458f6a625
unknown <shahajalalsheikh001@gmail.com>
2025-12-20 23:53:52 +0600

    fetch the url and show the details on the webpage

--- js/script.js
+++ js/script.js
@@ -2,9 +2,31 @@ fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
   .then((res) => res.json())
   .then((data) => {
     const container = document.getElementById("container");
-console.log(data.meals);
-    container.innerHTML = data.meals
-      .map((meal) => `<li>${meal.strCategory}</li>`)
-      .join("");
+    console.log(data.meals);
+    container.innerHTML = `
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-12 max-w-7xl mx-auto">
+        ${data.meals
+          .map(
+            (meal) => `
+              <div class="card bg-base-100 shadow-sm">
+                <figure>
+                  <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                </figure>
+                <div class="card-body">
+                  <h2 class="card-title">
+                    ${meal.strCategory}
+                  </h2>
+                  <p>${meal.strInstructions.slice(0, 200)}</p>
+                  <div class="card-actions justify-end">
+                    <div class="badge">
+                    <button class="btn bg-yellow-400">View Details</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `
+          )
+          .join("")}
+      </div>
+    `;
   });
-
